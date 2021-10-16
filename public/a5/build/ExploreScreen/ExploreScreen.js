e4222122a969b2b23fc3dec057afe391f17d301f
77 <ckq.0928@163.com>
2021-10-15 21:30:50 -0700

    a5 JavaScript fa21

--- public/a5/build/ExploreScreen/ExploreScreen.js
+++ public/a5/build/ExploreScreen/ExploreScreen.js
@@ -6,7 +6,7 @@ import WhoToFollowList from '../WhoToFollowList/index.js';
   $('#wd-explore').append(`
         <div class="row mt-2 ms-2">
         <div class="col-2 col-md-2 col-lg-2 col-xl-2">
-            ${NavigationSidebar()}
+            ${NavigationSidebar('explore')}
         </div>
         <div class="col-10 col-lg-6 col-xl-6">
             ${ExploreComponent()}
--- public/a5/build/NavigationSidebar/index.js
+++ public/a5/build/NavigationSidebar/index.js
@@ -1,69 +1,58 @@
-const explore = $("#wd-explore")
-const home = $("#wd-home")
-
 const NavigationSidebar = (active) => {
-  return (`
+    if (active === 'home') {
+        return (`
             <div class="list-group">
-                <a class="list-group-item" href="/">
+                <a class="list-group-item list-group-item-action" href="/">
                     <i class="fab fa-twitter"></i>
                     <span class="d-none d-xl-inline">
                 
                     </span>
                 </a>
             <!-- continue the rest of the list -->
-            if (active === home) {
-                <a class="list-group-item active" href="../HomeScreen/home.html">
+                <a class="list-group-item list-group-item-action active" href="../HomeScreen/home.html">
                     <i class="fas fa-home"></i>
                     <span class="d-none d-xl-inline">
                     Home
                     </span>
                 </a>
-            } else {
-                <a class="list-group-item" href="../HomeScreen/home.html">
-                    <i class="fas fa-home"></i>
-                    <span class="d-none d-xl-inline">
-                    Home
-                    </span>
-                </a>
-            }
 
-                <a class="list-group-item" href="../ExploreScreen/explore.html">
+                <a class="list-group-item list-group-item-action" href="../ExploreScreen/explore.html">
                     <i class="fas fa-hashtag"></i>
                     <span class="d-none d-xl-inline">
                         Explore
                     </span>    
                 </a>
-                <a class="list-group-item" href="/">
+                <a class="list-group-item list-group-item-action" href="/">
                     <i class="fas fa-bell"></i>
                     <span class="d-none d-xl-inline">
                         Notifications
                     </span> 
                 </a>
-                <a class="list-group-item" href="/">
+                <a class="list-group-item list-group-item-action" href="/">
                     <i class="fas fa-envelope"></i>
                     <span class="d-none d-xl-inline">
                         Messages
                     </span> 
                 </a>
-                <a class="list-group-item" href="/">
+                <a class="list-group-item list-group-item-action" href="/">
                     <i class="fas fa-bookmark"></i>
                     <span class="d-none d-xl-inline">
                         Bookmarks
                     </span> 
                 </a>
-                <a class="list-group-item" href="/">
+                <a class="list-group-item list-group-item-action" href="/">
                     <i class="fas fa-list-ul"></i>
                     <span class="d-none d-xl-inline">
                         Lists
                     </span>
                 </a>
-                <a class="list-group-item" href="/">
+                <a class="list-group-item list-group-item-action" href="/">
                     <i class="fas fa-user"></i>
                     <span class="d-none d-xl-inline">
                         Profile
                     </span>
                 </a>
-                <a class="list-group-item" href="/">
+                <a class="list-group-item list-group-item-action" href="/">
                     <i class="fas fa-comment-dots"></i>
                     <span class="d-none d-xl-inline">
                         More
@@ -76,5 +65,72 @@ const NavigationSidebar = (active) => {
                     Tweet</a>
             </div>
     `);
+    } else if (active === 'explore') {
+        return (`
+            <div class="list-group">
+                <a class="list-group-item list-group-item-action" href="/">
+                    <i class="fab fa-twitter"></i>
+                    <span class="d-none d-xl-inline">
+                
+                    </span>
+                </a>
+            <!-- continue the rest of the list -->
+                <a class="list-group-item list-group-item-action" href="../HomeScreen/home.html">
+                    <i class="fas fa-home"></i>
+                    <span class="d-none d-xl-inline">
+                    Home
+                    </span>
+                </a>
+                <a class="list-group-item list-group-item-action active" href="../ExploreScreen/explore.html">
+                    <i class="fas fa-hashtag"></i>
+                    <span class="d-none d-xl-inline">
+                        Explore
+                    </span>    
+                </a>
+                <a class="list-group-item list-group-item-action" href="/">
+                    <i class="fas fa-bell"></i>
+                    <span class="d-none d-xl-inline">
+                        Notifications
+                    </span> 
+                </a>
+                <a class="list-group-item list-group-item-action" href="/">
+                    <i class="fas fa-envelope"></i>
+                    <span class="d-none d-xl-inline">
+                        Messages
+                    </span> 
+                </a>
+                <a class="list-group-item list-group-item-action" href="/">
+                    <i class="fas fa-bookmark"></i>
+                    <span class="d-none d-xl-inline">
+                        Bookmarks
+                    </span> 
+                </a>
+                <a class="list-group-item list-group-item-action" href="/">
+                    <i class="fas fa-list-ul"></i>
+                    <span class="d-none d-xl-inline">
+                        Lists
+                    </span>
+                </a>
+                <a class="list-group-item list-group-item-action" href="/">
+                    <i class="fas fa-user"></i>
+                    <span class="d-none d-xl-inline">
+                        Profile
+                    </span>
+                </a>
+                <a class="list-group-item list-group-item-action" href="/">
+                    <i class="fas fa-comment-dots"></i>
+                    <span class="d-none d-xl-inline">
+                        More
+                    </span>
+                </a>
+            </div>
+            <div class="d-grid mt-2">
+                <a href="tweet.html"
+                   class="btn btn-primary btn-block rounded-pill">
+                    Tweet</a>
+            </div>
+    `);
+    }
+
 };
 export default NavigationSidebar;
