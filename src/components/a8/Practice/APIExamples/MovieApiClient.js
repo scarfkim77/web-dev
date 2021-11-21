1e79061f81e651b96ce8f72ee683fba3cb838603
77 <ckq.0928@163.com>
2021-11-20 23:26:18 -0800

    a8 fa21

--- src/components/a8/Practice/APIExamples/MovieApiClient.js
+++ src/components/a8/Practice/APIExamples/MovieApiClient.js
@@ -1,31 +1,29 @@
-import React, {useEffect, useState} from "react";
-const MovieApiClient = () => {
-    const [movies, setMovies] = useState([]);
+import React, {useEffect, useState} from 'react';
 
-    const deleteMovie = (movie) =>
-        fetch(`https://peaceful-wildwood-34209.herokuapp.com/api/movies/${movie._id}`, {
-            method: 'DELETE'
-        })
-        .then(response => response.json())
-        .then(movies => setMovies(movies));
-    const [id, setId] = useState(1000);
-    const [movie, setMovie] = useState({title: '', rating: 2.5, id: ''});
-    // const [id, setId] = useState(1000);
+const MovieApiClient = () => {
+    const [movie, setMovie] = useState({title: '', rating: 2.5});
     const onMovieTitleChange = (event) =>
         setMovie({...movie, title: event.target.value});
     const createMovieClickHandler = () =>
-        fetch('https://peaceful-wildwood-34209.herokuapp.com/api/movies', {
+        fetch('http://localhost:4000/api/movies', {
             method: 'POST',
-            body: JSON.stringify({...movie, _id: (new Date()).getTime() + ''}),
+            body: JSON.stringify(movie),
             headers: {
-                'content-type': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(movies => setMovies(movies));
-
+                'content-type': 'application/json',
+            },
+        }).then(response => response.json()).then(movies => setMovies(movies));
+    const [movies, setMovies] = useState([]);
+    useEffect(() =>
+            fetch('http://localhost:4000/api/movies').
+                then(response => response.json()).
+                then(movies => setMovies(movies))
+        , []);
+    const deleteMovie = (movie) =>
+        fetch(`http://localhost:4000/api/movies/${movie._id}`, {
+            method: 'DELETE',
+        }).then(response => response.json()).then(movies => setMovies(movies));
     const saveMovie = () =>
-        fetch(`https://peaceful-wildwood-34209.herokuapp.com/api/movies/${movie._id}`, {
+        fetch(`http://localhost:4000/api/movies/${movie._id}`, {
             method: 'PUT',
             body: JSON.stringify(movie),
             headers: {
@@ -34,34 +32,10 @@ const MovieApiClient = () => {
         })
         .then(response => response.json())
         .then(movies => setMovies(movies));
-
-    useEffect(() =>
-            fetch('https://peaceful-wildwood-34209.herokuapp.com/api/movies')
-            .then(response => response.json())
-            .then(movies => setMovies(movies))
-        , []);
-    return(
+    return (
         <div>
             <h2>Movies</h2>
             <ul className="list-group">
-                <li className="list-group-item">
-                    <button
-                        onClick={createMovieClickHandler}
-                        className="btn btn-success float-end">
-                        Create
-                    </button>
-                    <input className="form-control"
-                           value={movie.title}
-                           onChange={onMovieTitleChange}
-                           style={{width: "70%"}}/>
-                    <button
-                        onClick={saveMovie}
-                        className="btn btn-primary ms-2 float-end">
-                        Save
-                    </button>
-
-                </li>
-
                 {
                     movies.map((movie) =>
                         <li className="list-group-item"
@@ -71,17 +45,29 @@ const MovieApiClient = () => {
                                     className="btn btn-primary float-end ms-2">
                                 Edit
                             </button>
-
+                            <button
+                                onClick={createMovieClickHandler}
+                                className="btn btn-success float-end">
+                                Create
+                            </button>
+                            <input className="form-control"
+                                   value={movie.title}
+                                   onChange={onMovieTitleChange}
+                                   style={{width: '70%'}}/>
+                            <button
+                                onClick={saveMovie}
+                                className="btn btn-primary ms-2 float-end">
+                                Save
+                            </button>
                             <button onClick={() => deleteMovie(movie)}
                                     className="btn btn-danger float-end">
                                 Delete
                             </button>
-
-                        </li>
+                        </li>,
                     )
                 }
             </ul>
         </div>
-    )
+    );
 };
 export default MovieApiClient;
