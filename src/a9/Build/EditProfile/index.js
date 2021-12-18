ae991d477c613baa54543f8baaa27e5c986c1826
77 <ckq.0928@163.com>
2021-12-17 20:38:18 -0800

    a9 fa21

--- src/a9/Build/EditProfile/index.js
+++ src/a9/Build/EditProfile/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import './editProfile.css';
+import './editprofile.css';
 import {Link} from 'react-router-dom';
 import {editProfile} from '../../services/twitterService';
 
